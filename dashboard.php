b99bafa34facacc2e4e2628dd2212b21b610d935
Mwero Abdalla <mwerothewebmaker@gmail.com>
2023-12-10 17:49:26 +0300

    hh

--- dashboard.php
+++ dashboard.php
@@ -390,7 +390,7 @@ class="flex flex-col md:flex-row justify-center items-center p-20"
 
 <!-- Card 1: Bonus -->
 <div style="background-color: #ffffff; border: 1px solid #e0e0e0; "  class=" h-48 w-full md:w-1/2 lg:w-1/4 justify-between items-center p-5 flex-col gap-10">
-    <div style="" class ="flex flex-col justify-center items-center ">
+    
     <h3 style="margin: 0; color: black;">Bonus</h3>
         <!-- App Earnings Section -->          
             
@@ -452,7 +452,7 @@ echo '
             
       
 
-    </div>
+    
 </div> 
 <!-- //End of card 1  -->
 
